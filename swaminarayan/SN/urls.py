5604f8a484fab7e7dfe46d94dcc5278ff01eab4f
Malhar2004 <malharmr123@gmail.com>
2023-09-15 16:04:44 +0530

    commit

--- swaminarayan/SN/urls.py
+++ swaminarayan/SN/urls.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("reg/",views.register, name="reg"),
-    path("admin-login/",views.admin_login, name="admin-login"),
+    path("reg/", views.register, name="reg"),
+    path("admin-login/", views.admin_login, name="admin-login"),
 
 ]
