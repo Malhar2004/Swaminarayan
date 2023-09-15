a5f731275f663cf528d63e2e7d1733a873cf8a1f
Kaushal-80 <103827108+Kaushal-80@users.noreply.github.com>
2023-09-15 14:18:01 +0530

    1 commit

--- swaminarayan/SN/views.py
+++ swaminarayan/SN/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, "appointment/index.html")
+
+def register(request):
+    return render(request,"appointment/register.html")
+
+def admin_login(request):
+    return render(request, "appointment/admin_login.html")
